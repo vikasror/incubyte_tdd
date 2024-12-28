ef3bd93f2beea8cf0ed1ebd690e3c2ddd5c7c684
Vikas Dubey <vikasrordev@gmail.com>
2024-12-28 21:33:14 +0530

    Case12: Test pass because the add method return 10 with any length multiple delimiters

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -60,5 +60,9 @@ RSpec.describe StringCalculator do
     it 'allow multiple delimiters like this: (//[delim1][delim2]\n)' do
       expect(subject.add("//[*][%]\n1*2%3")).to eq(6)
     end
+
+    it 'allow any length multiple delimiters like this: (//[delim1][delim2]\n)' do
+      expect(subject.add("//[:::][%%]\n1:::2%%3:::4")).to eq(10)
+    end
   end
 end
